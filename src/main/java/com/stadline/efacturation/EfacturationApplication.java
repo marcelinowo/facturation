737e29b464bddee3c80fa380b178dbe6145b2238
marcelinowo <marcelinowo@gmail.com>
2019-11-16 17:13:10 +0100

    premier commit

---
+++ src/main/java/com/stadline/efacturation/EfacturationApplication.java
@@ -0,0 +1,13 @@
+package com.stadline.efacturation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfacturationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfacturationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stadline/efacturation/EfacturationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stadline.efacturation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfacturationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
